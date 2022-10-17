038e37801f994e225be307bb5239e3684eef1e49
dedojs <xandresousax@gmail.com>
2022-10-17 15:47:12 -0300

    area.py

---
+++ dia-1/area.py
@@ -0,0 +1,24 @@
+'''Boas pŕaticas'''
+'''2 linhas entre funções'''
+'''Declarar funções com letras minúsculas'''
+'''Dedclarar constantes com letras maíusculas'''
+
+PI = 3.14
+
+
+def square(side):
+    '''Calcular area do quadrado'''
+    return side * side
+
+
+def rectangle(length, width):
+    return length * width
+
+
+def circle(radius):
+    return PI * radius * radius
+
+
+print("Área do quadrado:", square(10))
+print("Área do retângulo:", rectangle(2, 2))
+print("Área do círculo", circle(3))
